9836399d9d65da91c939c396deabfc2994bd115d
poojithM <mpoojith@015@gmail.com>
2025-04-15 18:26:11 -0400

    Uploading files

---
+++ json_dataset/crew.py
@@ -0,0 +1,77 @@
+import json
+from pydantic import BaseModel, Field
+from crewai import Agent, Crew, Process, Task
+from crewai.project import CrewBase, agent, crew, task
+from crewai_tools import ScrapeWebsiteTool
+
+
+
+# If you want to run a snippet of code before or after the crew starts, 
+# you can use the @before_kickoff and @after_kickoff decorators
+# https://docs.crewai.com/concepts/crews#example-crew-class-with-decorators
+
+
+scraper = ScrapeWebsiteTool(website_url='https://www.mtu.edu/data-science/graduate/masters/')
+
+class Blog(BaseModel):
+    question: str = Field(..., description = "A question based on the scraped content")
+    answer: str = Field(..., description= "The answer to the question")
+
+@CrewBase
+class JsonDataset():
+	"""JsonDataset crew"""
+
+	# Learn more about YAML configuration files here:
+	# Agents: https://docs.crewai.com/concepts/agents#yaml-configuration-recommended
+	# Tasks: https://docs.crewai.com/concepts/tasks#yaml-configuration-recommended
+	agents_config = 'config/agents.yaml'
+	tasks_config = 'config/tasks.yaml'
+
+	
+	@agent
+	def scraper(self) -> Agent:
+		return Agent(
+			config=self.agents_config['scraper'],
+			tools = [scraper],
+			memory = True,
+   			verbose = True
+			
+		)
+
+	@agent
+	def reporting_analyst(self) -> Agent:
+		return Agent(
+			config=self.agents_config['reporting_analyst'],
+			verbose=True
+		)
+
+	# To learn more about structured task outputs, 
+	# task dependencies, and task callbacks, check out the documentation:
+	# https://docs.crewai.com/concepts/tasks#overview-of-a-task
+	@task
+	def scrape_task(self) -> Task:
+		return Task(
+			config=self.tasks_config['scrape_task'],
+		)
+
+	@task
+	def reporting_task(self) -> Task:
+		return Task(
+			config=self.tasks_config['reporting_task'],
+			output_json = Blog,
+			output_file='MTU_DataScience.json'
+		)
+
+	@crew
+	def crew(self) -> Crew:
+		"""Creates the JsonDataset crew"""
+		# To learn how to add knowledge sources to your crew, check out the documentation:
+		# https://docs.crewai.com/concepts/knowledge#what-is-knowledge
+
+		return Crew(
+			agents=self.agents, # Automatically created by the @agent decorator
+			tasks=self.tasks, # Automatically created by the @task decorator
+			process=Process.sequential,
+			verbose=True,
+			# process=Process.hierarchical, # In case you wanna use that instead https://docs.crewai.com/how-to/Hierarchical/
+		)
---
+++ json_dataset/main.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+import sys
+import warnings
+
+from datetime import datetime
+
+from crew import JsonDataset
+
+warnings.filterwarnings("ignore", category=SyntaxWarning, module="pysbd")
+
+# This main file is intended to be a way for you to run your
+# crew locally, so refrain from adding unnecessary logic into this file.
+# Replace with inputs you want to test with, it will automatically
+# interpolate any tasks and agents information
+
+def run():
+    """
+    Run the crew.
+    """
+    
+    try:
+        JsonDataset().crew().kickoff()
+    except Exception as e:
+        raise Exception(f"An error occurred while running the crew: {e}")
+
+
+run()
